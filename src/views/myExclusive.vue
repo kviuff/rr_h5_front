c08cae6e9d922044bc7e7dabf1f74e0f7aa83c98
kviuff <kanglan@leimingtech.com>
2019-10-31 00:28:04 +0800

    1、修改注册页面提示 2、修改生成的二维码无法扫描成功

--- src/views/myExclusive.vue
+++ src/views/myExclusive.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="myExclusive">
-         <!-- <img class="bgimg topImg" id="bgimg" src="static/img/bg3.jpg" alt=""> -->
-          <!--<div class="wordWarp">
+    <!-- <img class="bgimg topImg" id="bgimg" src="static/img/bg3.jpg" alt=""> -->
+    <!--<div class="wordWarp">
             <div class=" topWord1">
                 <p>您已经接受劳斯莱斯 BLACK BADGE</p>
                 <p>黑暗觉醒 无限远征的召集，</p>
@@ -11,199 +11,206 @@
             <div class="canvasWarp">
                 <img  src="static/img/canvasWarp.png" alt="">
             </div>
-        </div>  -->
-         <!-- <img  src="static/img/canvasWarp.png" alt=""> -->
-        <canvas id="myCanvas" width="750" height="1334"></canvas>
-        <div id="qrCode" ref="qrCodeDiv"></div>
-        <img id="endimg" v-if="endimg" class="saveEndimg"  :src="endimg"   alt="">
-
+    </div>-->
+    <!-- <img  src="static/img/canvasWarp.png" alt=""> -->
+    <canvas id="myCanvas" width="750" height="1334"></canvas>
+    <div id="qrCode" ref="qrCodeDiv"></div>
+    <img id="endimg" v-if="endimg" class="saveEndimg" :src="endimg" alt />
   </div>
 </template>
 
 <script>
-import QRCode from 'qrcodejs2';
-import {saveImage} from "@/api/api"
-import { Toast } from 'vant';
+import QRCode from "qrcodejs2";
+import { saveImage } from "@/api/api";
+import { Toast } from "vant";
 export default {
-    name: "HelloWorld",
-    data() {
-        return {
-            endimg:"",
-        }
+  name: "HelloWorld",
+  data() {
+    return {
+      endimg: ""
+    };
+  },
+  created() {
+    Toast.clear();
+  },
+  mounted() {
+    if (this.$route.query.id) {
+      this.$nextTick(() => {
+        this.drawCanvas();
+      });
+    } else {
+      this.$router.replace({
+        path: "regist"
+      });
+    }
+  },
+  methods: {
+    // 生成二维码
+    makeQrcode() {
+      document.getElementById("qrCode").innerHTML = "";
+      // var url = "http://xxxxx.com/f/member/api/code/777777777"
+      var id = this.$route.query.id;
+      // var a = new QRCode(this.$refs.qrCodeDiv, {
+      //     text: id,
+      //     width: 200,
+      //     height: 200,
+      //     colorDark: "#333333", //二维码颜色
+      //     colorLight: "#ffffff", //二维码背景色
+      //     correctLevel: QRCode.CorrectLevel.L//容错率，L/M/H
+      // })
+      // alert(JSON.stringify(a));
+      //生成前景色为红色背景色为白色的二维码
+      jQuery("#qrCode").qrcode({
+        text: id,
+        render: "canvas", //也可以替换为table
+        foreground: "#333333",
+        background: "#ffffff",
+        width: 200,
+        height: 200
+      });
     },
-    created(){
-       Toast.clear();
-    },
-    mounted() {
-        
-         if(this.$route.query.id){
-           this.$nextTick(()=>{
-                this.drawCanvas();
-           })
-         }else{
-            this.$router.replace({
-                path:"regist",
-            })
-        }
-    },
-    methods:{
-         // 生成二维码
-        makeQrcode() {
-             document.getElementById('qrCode').innerHTML = ''
-            // var url = "http://xxxxx.com/f/member/api/code/777777777"
-            var id = this.$route.query.id
-            // var a = new QRCode(this.$refs.qrCodeDiv, {
-            //     text: id,
-            //     width: 200,
-            //     height: 200,
-            //     colorDark: "#333333", //二维码颜色
-            //     colorLight: "#ffffff", //二维码背景色
-            //     correctLevel: QRCode.CorrectLevel.L//容错率，L/M/H
-            // })
-            // alert(JSON.stringify(a));
-            //生成前景色为红色背景色为白色的二维码
-            jQuery('#qrCode').qrcode({
-                text: id,
-                render: "canvas", //也可以替换为table
-                foreground: "#333333",
-                background: "#ffffff",
-                width: 200,
-                height: 200,
-            });
-        },
-        // canvas画图
-        drawCanvas(){
-            let that = this;
-            var c=document.getElementById("myCanvas");
-            var ctx=c.getContext("2d");
-            // 画背景图
-            // let bgImg=document.getElementById("bgimg");
-            let bgImg = new Image ()
-            bgImg.crossOrigin = 'Anonymous'
-            bgImg.src = "static/img/bg3.jpg"
-            bgImg.onload = ()=>{
-                ctx.drawImage(bgImg,0,0,1080,1920,0,0,750,1334)
-                  // 写字
-                // ctx.font="30px  'Gill Alt One MT'";
-                ctx.font="30px Gill Alt One MT";
-                ctx.fillStyle="white";
-                ctx.fillText("您已经接受劳斯莱斯 BLACK BADGE",137.5,913.27);
-                ctx.fillText("黑暗觉醒 无限远征的召集，",195.8333,951.394);
-                ctx.fillText("请您携带好入场凭证， ",229.16667,993.05556);
-                ctx.fillText("与我们一同感受BLACK BADGE的神秘力量",93.75,1037.5);
-            
-                // 二维码外边框
-                let qrcodeOut = new Image ()
-                qrcodeOut.crossOrigin = 'Anonymous'
-                qrcodeOut.src =  "static/img/canvasWarp.png";
-                qrcodeOut.onload = ()=>{
-                    ctx.drawImage(qrcodeOut,0,0,234,234,300.72,1126.5,164,164)
-                   // 画二维码
-                    this.$nextTick ( () => {
-                        this.makeQrcode();
-                        setTimeout(()=>{
-                            let qrcode = new Image ()
-                            qrcode.crossOrigin = 'Anonymous'
-                            var qrcodesrc  = document.querySelector('#qrCode canvas').toDataURL("image/png");
-                            qrcode.src = qrcodesrc;
-                            qrcode.onload = ()=>{
-                                ctx.drawImage(qrcode,0,0,200,200,309.72,1137.5,141.89,141.89)
-                                // ctx.drawImage(img,0,0,100,100,100,100);
-                                var strDataURI=c.toDataURL("image/png");    
-                                // console.log();
-                                that.uploadImg(strDataURI);
-                                // that.uploadImg(qrcodesrc);
-                            }
-                        },500)
-                    })
+    // canvas画图
+    drawCanvas() {
+      let that = this;
+      var c = document.getElementById("myCanvas");
+      var ctx = c.getContext("2d");
+      // 画背景图
+      // let bgImg=document.getElementById("bgimg");
+      let bgImg = new Image();
+      bgImg.crossOrigin = "Anonymous";
+      bgImg.src = "static/img/bg3.jpg";
+      bgImg.onload = () => {
+        ctx.drawImage(bgImg, 0, 0, 1080, 1920, 0, 0, 750, 1334);
+        // 写字
+        // ctx.font="30px  'Gill Alt One MT'";
+        ctx.font = "30px Gill Alt One MT";
+        ctx.fillStyle = "white";
+        ctx.fillText("您已经接受劳斯莱斯 BLACK BADGE", 137.5, 913.27);
+        ctx.fillText("黑暗觉醒 无限远征的召集，", 195.8333, 951.394);
+        ctx.fillText("请您携带好入场凭证， ", 229.16667, 993.05556);
+        ctx.fillText("与我们一同感受BLACK BADGE的神秘力量", 93.75, 1037.5);
 
-                }
-            }
-        },
-        // base64上传图片
-        uploadImg(base64){
-            var obj = {
-                pictureBase64:base64,
-                memberId:this.$route.query.id
-            }
-            // 自定义加载图标
-            Toast.loading({
-                duration: 0, // 持续展示 toast
-                message: '生成专属图中...',
-                forbidClick: true,
-                loadingType: 'spinner'
-            });
-            saveImage(obj).then((res)=>{
-                Toast.clear();
-                if(res.result=="true"){
-                    this.endimg = res.data.imagePath
-                    // Toast.success(res.message);
-                }else{
-                    TToast.success(res.message);
-                }
-            })
-        },
-        
+        // 二维码外边框
+        let qrcodeOut = new Image();
+        qrcodeOut.crossOrigin = "Anonymous";
+        qrcodeOut.src = "static/img/1572450050201.jpg";
+        qrcodeOut.onload = () => {
+          ctx.drawImage(qrcodeOut, 0, 0, 234, 214, 265.72, 1106.5, 220, 220);
+          // 画二维码
+          this.$nextTick(() => {
+            this.makeQrcode();
+            setTimeout(() => {
+              let qrcode = new Image();
+              qrcode.crossOrigin = "Anonymous";
+              var qrcodesrc = document
+                .querySelector("#qrCode canvas")
+                .toDataURL("image/png");
+              qrcode.src = qrcodesrc;
+              qrcode.onload = () => {
+                ctx.drawImage(
+                  qrcode,
+                  0,
+                  0,
+                  200,
+                  200,
+                  275.72,
+                  1117.5,
+                  200.89,
+                  200.89
+                );
+                // ctx.drawImage(img,0,0,100,100,100,100);
+                var strDataURI = c.toDataURL("image/png");
+                // console.log();
+                that.uploadImg(strDataURI);
+                // that.uploadImg(qrcodesrc);
+              };
+            }, 500);
+          });
+        };
+      };
     },
-   
+    // base64上传图片
+    uploadImg(base64) {
+      var obj = {
+        pictureBase64: base64,
+        memberId: this.$route.query.id
+      };
+      // 自定义加载图标
+      Toast.loading({
+        duration: 0, // 持续展示 toast
+        message: "生成专属图中...",
+        forbidClick: true,
+        loadingType: "spinner"
+      });
+      saveImage(obj).then(res => {
+        Toast.clear();
+        if (res.result == "true") {
+          this.endimg = res.data.imagePath;
+          // Toast.success(res.message);
+        } else {
+          TToast.success(res.message);
+        }
+      });
+    }
+  }
 };
 </script>
 <style scoped>
-#qrCode{
-    display: none;
+#qrCode {
+  display: none;
 }
 /* (895/(1080/750))/75 */
-.myExclusive{
-    width: 100%;
-    height: 100%;;
-    /* background-color:#000000;  */
-    color: #ffffff;
-    overflow: hidden;
+.myExclusive {
+  width: 100%;
+  height: 100%;
+  /* background-color:#000000;  */
+  color: #ffffff;
+  overflow: hidden;
 }
-.myExclusive .bgimg{
-    /* position: absolute;
+.myExclusive .bgimg {
+  /* position: absolute;
     top: 0;
     left: 0;
     width:100%; */
- /* height: 9.89815rem; */
-   /* opacity: 0; */
-   display: none;
+  /* height: 9.89815rem; */
+  /* opacity: 0; */
+  display: none;
 }
-.myExclusive .wordWarp{
-    position: relative;
-    top:12.056rem;
-    text-align: center;
-    font-size: 0.3704rem;
+.myExclusive .wordWarp {
+  position: relative;
+  top: 12.056rem;
+  text-align: center;
+  font-size: 0.3704rem;
 }
-.myExclusive .topWord1{
-    height: 2.037rem;
-    line-height: 0.50925rem;
+.myExclusive .topWord1 {
+  height: 2.037rem;
+  line-height: 0.50925rem;
 }
-.myExclusive .canvasWarp{
-    position: relative;
-    top: 0.20833rem;
-    width: 100%;
-    text-align: center;
+.myExclusive .canvasWarp {
+  position: relative;
+  top: 0.20833rem;
+  width: 100%;
+  text-align: center;
 }
-.myExclusive .canvasWarp img{
-    width:2.5rem;
-    height:2.5rem;
+.myExclusive .canvasWarp img {
+  width: 2.5rem;
+  height: 2.5rem;
 }
 
-#myCanvas{
-    width: 100%;
-    height: auto;
-    position: absolute;
-    left: 0;
-    bottom: 0;
+#myCanvas {
+  width: 100%;
+  height: auto;
+  position: absolute;
+  left: 0;
+  bottom: 0;
 }
-.saveEndimg{
-    z-index:1000;
-    position: absolute;
-    top: 0;
-    left: 0;
-    width:100%;
-    height:100%;
-    opacity: 0;
+.saveEndimg {
+  z-index: 1000;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
 }
 </style>
--- src/views/regist.vue
+++ src/views/regist.vue
@@ -1,269 +1,271 @@
 <template>
   <div class="registWarp">
-         <img class="topImg" src="static/img/bg2.jpg" alt="">
-         <div class="registInfo" style="background-image:url('static/img/bg2_bg1.jpg');">
-            <div style="width:1rem;height:0.8333rem;"></div> 
-            <div class="registItem">
-                <p>称谓</p>
-                <select class="select" v-model="mtitle" :class="mtitle==''?'grayColor':''">
-                    <option value='' disabled selected style="display:none;">请选择您的称谓</option>
-                    <option value ="男">男士</option>
-                    <option value ="女">女士</option>
-                </select>
-            </div>
-            <div  class="registItem">
-                <p>姓名</p>
-                 <input type="text" v-model="mname" placeholder="请填写您的姓名" >
-            </div>
-            <div  class="registItem">
-                <p>手机号码</p>
-                 <input type="text"  v-model="mmobile" maxlength="11" placeholder="请填写您的手机号码" >
-            </div>
-            <div  class="registItem">
-                <p>预约观展日期</p>
-                <select class="select"  v-model="mdate" :class="mdate==''?'grayColor':''">
-                    <option value='' disabled selected style="display:none;">请选择您的预约观展日期</option>
-                    <option value ="2019-11-16 00:00:00">2019年11月16日</option>
-                    <option value ="2019-11-17 00:00:00">2019年11月17日</option>
-                    <option value ="2019-11-18 00:00:00">2019年11月18日</option>
-                </select>
-            </div>
-            <div class="bottomWarp" >
-                <ul class="ulWarp" style="list-style-type:disc">
-                    <li>2019 劳斯莱斯BLACK BADGE品牌展参观券可在活 动期间内的任何时段使用，过期无效。</li>
-                    <li>本券为2019 劳斯莱斯BLACK BADGE品牌展唯一入场凭证，仅限本人使用，不可擅自转让或租借他人。</li>
-                    <li>个人信息仅作为本次活动入场确认使用。<br>劳斯莱斯汽车保留在法律范围内对本活动的解释权。</li>
-                </ul>
+    <img class="topImg" src="static/img/bg2.jpg" alt />
+    <div class="registInfo" style="background-image:url('static/img/bg2_bg1.jpg');">
+      <div style="width:1rem;height:0.8333rem;"></div>
+      <div class="registItem">
+        <p>称谓</p>
+        <select class="select" v-model="mtitle" :class="mtitle==''?'grayColor':''">
+          <option value disabled selected style="display:none;">请选择您的称谓</option>
+          <option value="男">男士</option>
+          <option value="女">女士</option>
+        </select>
+      </div>
+      <div class="registItem">
+        <p>姓名</p>
+        <input type="text" v-model="mname" placeholder="请填写您的姓名" />
+      </div>
+      <div class="registItem">
+        <p>手机号码</p>
+        <input type="text" v-model="mmobile" maxlength="11" placeholder="请填写您的手机号码" />
+      </div>
+      <div class="registItem">
+        <p>预约观展日期</p>
+        <select class="select" v-model="mdate" :class="mdate==''?'grayColor':''">
+          <option value disabled selected style="display:none;">请选择您的预约观展日期</option>
+          <option value="2019-11-16 00:00:00">2019年11月16日</option>
+          <option value="2019-11-17 00:00:00">2019年11月17日</option>
+          <option value="2019-11-18 00:00:00">2019年11月18日</option>
+        </select>
+      </div>
+      <div class="bottomWarp">
+        <ul class="ulWarp" style="list-style-type:disc">
+          <li>2019 劳斯莱斯BLACK BADGE品牌展参观券可在活 动期间内的任何时段使用，过期无效。</li>
+          <li>本券为2019 劳斯莱斯BLACK BADGE品牌展唯一入场凭证，仅限本人使用，不可擅自转让或租借他人。</li>
+          <li>
+            个人信息仅作为本次活动入场确认使用。
+            <br />劳斯莱斯汽车保留在法律范围内对本活动的解释权。
+          </li>
+        </ul>
 
-                <div class="checkboxWarp">
-                    <!-- <van-checkbox v-model="checked">
-                        <img 
+        <div class="checkboxWarp">
+          <!-- <van-checkbox v-model="checked">
+                        <img
                             style="width:0.463rem;height:0.463rem"
                             slot="icon"
                             slot-scope="props"
                             :src="props.checked ? activeIcon : inactiveIcon"
-                        >  
-                        
-                    </van-checkbox> -->
-                    <van-checkbox v-model="checked"  shape="square" checked-color="#ffa70e">
-                        <label style="color:#808080;margin-left:0.278rem;">我已阅读并同意以上内容</label>
-                    </van-checkbox>
-                </div>
-                <div class="submitWarp">
-                    <!-- <van-button type="default" class="submmitbt" @click="goMyExclusive">
+                        >
+
+          </van-checkbox>-->
+          <van-checkbox v-model="checked" shape="square" checked-color="#ffa70e">
+            <label style="color:#808080;margin-left:0.278rem;">我已阅读并同意以上内容</label>
+          </van-checkbox>
+        </div>
+        <div class="submitWarp">
+          <!-- <van-button type="default" class="submmitbt" @click="goMyExclusive">
                         <span>提</span>
                         <span>交</span>
-                    </van-button> -->
-                    <img class="submmitbt" @click="goMyExclusive" src="static/img/submit.png" alt="">
-                </div>
-            </div>
-         </div>
+          </van-button>-->
+          <img class="submmitbt" @click="goMyExclusive" src="static/img/submit.png" alt />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import {saveVip} from "@/api/api"
-import { Toast } from 'vant';
+import { saveVip } from "@/api/api";
+import { Toast } from "vant";
 export default {
   name: "HelloWorld",
   data() {
     return {
-        value:"",
-        checked:false,
-        activeIcon: 'https://img.yzcdn.cn/vant/user-active.png',
-        inactiveIcon: 'https://img.yzcdn.cn/vant/user-inactive.png',
-        mtitle:'',
-        mname:'',
-        mmobile:'',
-        mdate:'',
-    }
+      value: "",
+      checked: false,
+      activeIcon: "https://img.yzcdn.cn/vant/user-active.png",
+      inactiveIcon: "https://img.yzcdn.cn/vant/user-inactive.png",
+      mtitle: "",
+      mname: "",
+      mmobile: "",
+      mdate: ""
+    };
   },
-    created(){
-      Toast.clear();
-    },
-    mounted() {
-    },
-    methods:{
-        // 去注册
-        goMyExclusive(){
-            if(!this.mtitle){
-                Toast.fail('请选择称谓');
-                 return;
-            }
-            if(!this.mname){
-                 Toast.fail('请填写姓名');
-                 return;
-            }
-            if(!/^1\d{10}$/.test(this.mmobile)){
-                 Toast.fail('请填写正确的手机号');
-                 return;
-            }
-            if(!this.mdate){
-                 Toast.fail('请选择预约观展日期');
-                return;
-            }
-            if(!this.checked){
-              Toast.fail('请同意以上内容');
-                return
-            }
-            // this.mtitle = "男"
-            // this.mname = "bai4"
-            // this.mmobile = "13244169064"
-            // this.mdate = "2019-11-16 00:00:00"
-            var obj = {
-                mtitle:this.mtitle,
-                mname:this.mname,
-                mmobile:this.mmobile,
-                mdate:this.mdate,
-            }
-            saveVip(obj).then((res)=>{
-                if(res.result=="true" && res.data && res.data.id){
-                    this.$router.push({
-                        path:"myExclusive",
-                        query:{id:res.data.id},
-                    })
-                    Toast.success(res.message);  
-                }else{
-                    Toast.fail(res.message);
-                }
-            })
-           
+  created() {
+    Toast.clear();
+  },
+  mounted() {},
+  methods: {
+    // 去注册
+    goMyExclusive() {
+      if (!this.mtitle) {
+        Toast.fail("请您补充完整信息");
+        return;
+      }
+      if (!this.mname) {
+        Toast.fail("请您补充完整信息");
+        return;
+      }
+      if (!/^1\d{10}$/.test(this.mmobile)) {
+        Toast.fail("请您补充完整信息");
+        return;
+      }
+      if (!this.mdate) {
+        Toast.fail("请您补充完整信息");
+        return;
+      }
+      if (!this.checked) {
+        Toast.fail("请您补充完整信息");
+        return;
+      }
+      // this.mtitle = "男"
+      // this.mname = "bai4"
+      // this.mmobile = "13244169064"
+      // this.mdate = "2019-11-16 00:00:00"
+      var obj = {
+        mtitle: this.mtitle,
+        mname: this.mname,
+        mmobile: this.mmobile,
+        mdate: this.mdate
+      };
+      saveVip(obj).then(res => {
+        if (res.result == "true" && res.data && res.data.id) {
+          this.$router.push({
+            path: "myExclusive",
+            query: { id: res.data.id }
+          });
+          Toast.success(res.message);
+        } else {
+          Toast.fail(res.message);
         }
-    },
+      });
+    }
+  }
 };
 </script>
 <style scoped>
 /* (895/(1080/750))/75 */
-.registWarp{
-    width: 100%;
-    height: 100%;
-    color: #ffffff;
-    position: relative;
-    overflow: hidden;
-    /* 
+.registWarp {
+  width: 100%;
+  height: 100%;
+  color: #ffffff;
+  position: relative;
+  overflow: hidden;
+  /*
     position: absolute;
     bottom: 0;
     left:0;
-    background-color:#000000; 
+    background-color:#000000;
     background-color:red; */
 }
-.registWarp, .topImg{
-    position: absolute;
-    top: 0;
-    left: 0;
-    width:100%;
- /* height: 9.89815rem; */
+.registWarp,
+.topImg {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  /* height: 9.89815rem; */
 }
-.registWarp .registInfo{
-    position: absolute;
-    bottom:1rem;
-    left: 0.55rem;
-    /* padding-top: 0.8333rem; */
-    text-align: center;
-    font-size: 0.3704rem;
-    background: #000000;
-    width:8.803703703rem;
-    margin: auto;
-     background-size: 100%;
-    background-repeat: no-repeat;
+.registWarp .registInfo {
+  position: absolute;
+  bottom: 1rem;
+  left: 0.55rem;
+  /* padding-top: 0.8333rem; */
+  text-align: center;
+  font-size: 0.3704rem;
+  background: #000000;
+  width: 8.803703703rem;
+  margin: auto;
+  background-size: 100%;
+  background-repeat: no-repeat;
 }
-.registItem{
-    width: 7.46296rem;
-    height: 1.2037rem;
-    margin: auto;
-    margin-top:0.31481rem;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    border-bottom: 0.00926rem solid #808080;
+.registItem {
+  width: 7.46296rem;
+  height: 1.2037rem;
+  margin: auto;
+  margin-top: 0.31481rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  border-bottom: 0.00926rem solid #808080;
 }
-.registItem p{
-  width:7.1388rem;
+.registItem p {
+  width: 7.1388rem;
   text-align: left;
   height: 0.46296rem;
 }
-.bottomWarp{
-    /* background: black;
+.bottomWarp {
+  /* background: black;
     width: 98%;
     margin-left: 1%; */
 }
-input,select{
+input,
+select {
   background-color: black;
-
 }
-input,select,.ulWarp{
-  width:7.1388rem;
+input,
+select,
+.ulWarp {
+  width: 7.1388rem;
   margin: auto;
 
   /* padding-left: 0.5rem;
   padding-top: 0.5rem; */
   color: white;
 }
-.van-cell{
-   background-color: black;
+.van-cell {
+  background-color: black;
 }
-.checkboxWarp{
-  width:7.1388rem;
+.checkboxWarp {
+  width: 7.1388rem;
   margin: auto;
 }
-/deep/ .van-checkbox__icon{
-    height: auto !important;
-   box-shadow:0px 0px 5px red;
+/deep/ .van-checkbox__icon {
+  height: auto !important;
+  box-shadow: 0px 0px 5px red;
 }
-/deep/  .van-checkbox__icon .van-icon{
-    border-color: #ffa70e !important;
+/deep/ .van-checkbox__icon .van-icon {
+  border-color: #ffa70e !important;
 }
-/deep/ .van-checkbox__label{
-    height: 0.34rem;
-    line-height: 0.34rem;
-}
-
-.grayColor{
-    color:#808080
+/deep/ .van-checkbox__label {
+  height: 0.34rem;
+  line-height: 0.34rem;
 }
 
+.grayColor {
+  color: #808080;
+}
 
-.ulWarp{
-    height: 1.94444rem;
-    text-align: left;
-    margin-top:0.462962rem;
+.ulWarp {
+  height: 1.94444rem;
+  text-align: left;
+  margin-top: 0.462962rem;
 }
-.ulWarp li{
-    font-size: 0.277777rem;
-    height:0.69rem;
-    line-height: 0.34rem;
-    letter-spacing: 0.00925rem;
+.ulWarp li {
+  font-size: 0.277777rem;
+  height: 0.69rem;
+  line-height: 0.34rem;
+  letter-spacing: 0.00925rem;
 }
-.checkboxWarp{
-    margin-top:0.5555566rem
+.checkboxWarp {
+  margin-top: 0.5555566rem;
 }
-.submitWarp{
-    margin-top:0.8334rem;
+.submitWarp {
+  margin-top: 0.8334rem;
 }
-.submmitbt{
-    width: 3.407rem;
-    /* width: 1.7724rem; */
-    /* height: 0.5rem;
+.submmitbt {
+  width: 3.407rem;
+  /* width: 1.7724rem; */
+  /* height: 0.5rem;
     line-height: 0.4rem; */
-    height: auto;
-    border-radius: 0.0926rem;
-   
+  height: auto;
+  border-radius: 0.0926rem;
 }
-.submmitbt .van-button__text{
-    width: 1.148148rem;
-    display: inline-flex;
-    justify-content: space-between;
-}
-
-select{
-    appearance:none;
-    -moz-appearance:none;
-    -webkit-appearance:none;
-    border:none;
-    background: url("../../static/img/downarrow.png") no-repeat scroll right center transparent;
-    /* border: 10px solid #ccc; */
-    padding-left: 0.186667rem;
-    background-size: 0.3rem;
+.submmitbt .van-button__text {
+  width: 1.148148rem;
+  display: inline-flex;
+  justify-content: space-between;
 }
 
+select {
+  appearance: none;
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  border: none;
+  background: url("../../static/img/downarrow.png") no-repeat scroll right
+    center transparent;
+  /* border: 10px solid #ccc; */
+  padding-left: 0.186667rem;
+  background-size: 0.3rem;
+}
 </style>
